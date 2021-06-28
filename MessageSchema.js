5a3160927ddedbdf3de57f2b8b773abef2f8f1c8
Petar Ivanov <peteriv9606@gmail.com>
2021-06-28 15:41:17 +0300

    should be working with heroku now

--- MessageSchema.js
+++ MessageSchema.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const MessageSchema = Schema({
-  userId: { type: String, required: true },
-  message: { type: String, required: true },
-  date: { type: Date, required: true },
-});
+const MessageSchema = Schema([
+  {
+    userId: { type: String, required: true },
+    message: { type: String, required: true },
+    date: { type: Date, required: true },
+  },
+]);
 
 module.exports = MessageSchema;
